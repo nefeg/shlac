94d5bea659b3e51badc9ceb9c843282c5089b461
evgeny.nefedkin <evgeny.nefedkin@umbrella-web.com>
2018-03-02 02:40:47 +0300

    Review 0.6 (adapting to PPA)

---
+++ Makefile
@@ -0,0 +1,121 @@
+## These will be provided to the target
+NAME=shlac
+VERSION=0.6-0ubuntu1
+SOURCE=https://verefkin@bitbucket.org/verefkin/shlac.git
+PPA=ppa:onm/shlac
+
+BUILD = $(shell date +%s)
+
+## Use linker flags to provide version/build settings to the target
+LDFLAGS=-ldflags "-X=main.Version=$(VERSION) -X=main.Build=$(BUILD)"
+
+
+
+SHELL = /bin/bash
+GOPATH = $(shell pwd)
+GOBIN = $(GOPATH)/bin
+TARGET1 = $(NAME)d
+TARGET2 = $(NAME)
+BIN	= $(DESTDIR)/usr/bin
+CONF = $(DESTDIR)/etc/shlac
+
+
+export PATH := $(PATH):/usr/local/go/bin
+
+.PHONY: all install uninstall clean ppa configure ppa_test configure_test
+
+all:
+	@echo "##################################"
+	@echo "# 	Compile binaries"
+	@echo "##################################"
+	@echo $(LDFLAGS)
+
+	mkdir -p $(GOBIN)
+	GOPATH=$(GOPATH) GOBIN=$(GOBIN) go install $(LDFLAGS) $(TARGET1) $(TARGET2)
+
+	@echo " *** Done ***"
+	@echo ""
+
+
+install: all
+
+	mkdir -p $(BIN)
+
+	@echo " *** Install server"
+	install $(GOBIN)/$(TARGET1) $(BIN)
+
+	@echo " *** Install client"
+	install $(GOBIN)/$(TARGET2) $(BIN)
+
+	@echo " *** Install config"
+	mkdir -p $(CONF)
+	install ./config.json $(CONF)/config.json
+
+
+uninstall:
+	rm -rf $(BIN)/$(TARGET1)
+	rm -rf $(BIN)/$(TARGET2)
+	rm -rf $(CONF)/config.json
+
+
+configure_test:
+	@echo "##############################"
+	@echo "# Compile TEST build"
+	@echo "##############################"
+
+	$(MAKE) clean
+	./configure_ppa.sh shlac $(VERSION) 1 https://verefkin@bitbucket.org/verefkin/shlac.git
+
+	cd build/tmp; debuild  -S -us -uc
+
+	@echo " *** Build(TEST) is compiled ***"
+	@echo ""
+
+
+configure:
+	@echo "###############################"
+	@echo "# Compile build"
+	@echo "###############################"
+
+	@echo " ==> Cleaning build directory..."
+	$(MAKE) clean
+
+	@echo " ==> Configure build..."
+	./configure_ppa.sh shlac $(VERSION) 0 https://verefkin@bitbucket.org/verefkin/shlac.git
+
+	### build package (https://help.launchpad.net/Packaging/PPA/BuildingASourcePackage)
+	cd build/tmp; debuild -S -sa
+
+	@echo " *** Build is compiled ***"
+	@echo ""
+
+
+ppa: ppa_test configure
+
+	@echo " ==> Uploading to PPA..."
+	dput -d $(PPA) $(shell ls build/*.changes)
+
+	$(MAKE) clean
+
+
+ppa_test: configure_test
+	@echo "###############################"
+	@echo "#**** 	TESTING BUILD	 ****#"
+	@echo "###############################"
+
+	@echo " ==> Unpacking..."
+	cd build/; dpkg-source -x *.dsc
+
+	@echo " ==> Testing..."
+	cd build/$(NAME)-$(VERSION); dh_auto_test;
+
+	@echo " ==> Compiling..."
+	cd build/$(NAME)-$(VERSION); dh_auto_build -a
+
+	@echo " *** Build(TEST) is OK ***"
+	@echo ""
+	$(MAKE) clean
+
+
+clean:
+	@rm -rf build/
---
+++ configure_ppa.sh
@@ -0,0 +1,122 @@
+#!/usr/bin/env bash
+# project root
+#   |_src
+#   |_WORK_DIR
+#       |_SOURCE_DIR
+#       |_TMP_DIR
+#       |_BUILD_NAME.tar.gz
+#
+#
+
+#
+# Usage ./this-script <project name> <version> <isTest> <project source>
+# Example: ./this-script shlac 0.25-0ubuntu1 0 https://verefkin@bitbucket.org/verefkin/shlac.git
+#
+
+# date ; sudo service ntp stop ; sudo ntpdate -s time.nist.gov ; sudo service ntp start ; date
+#
+# *** DIR: %project root%
+#
+
+###################################
+#   checking version              #
+###################################
+if [[ $1 == '' ]];then
+    echo "expected version (ex.: 0.25-0ubuntu1)"
+    exit 1
+fi
+
+PROJECT_NAME=$1
+VERSION="$2"
+IS_TEST="$3"
+PROJECT_SOURCE=$(echo $4|sed 's/\//\\\//g')
+
+
+BUILD_NAME="${PROJECT_NAME}-${VERSION}"
+BUILD_NAME_ALT="${PROJECT_NAME}_${VERSION}"
+WORK_DIR=$(pwd)"/build"
+TMP_DIR=$WORK_DIR/tmp
+SOURCE_DIR=$WORK_DIR/source
+
+export DEBEMAIL="evgeny.nefedkin@umbrella-web.com"
+export DEBFULLNAME="Evgeny Nefedkin"
+
+
+echo "$PROJECT_NAME ($BUILD_NAME_ALT)"
+
+###################################
+#   making workspace              #
+###################################
+#git archive --format=tar.gz -o $WORK_DIR/$BUILD_NAME.tar.gz ppa
+## copy sources
+mkdir -p $SOURCE_DIR
+cp -rf src/ $SOURCE_DIR/src/
+cp -rf $SOURCE_DIR/src/vendor/* $SOURCE_DIR/src/
+
+cp Makefile $SOURCE_DIR/Makefile
+cp config.json $SOURCE_DIR/config.json
+cp readme.md $SOURCE_DIR/readme.md
+
+## remove .git's directories
+find $SOURCE_DIR/ -name .git | rm -fr
+
+## make archive
+#
+# *** DIR: %project root%/$WORK_DIR/$SOURCE_DIR
+#
+cd $SOURCE_DIR
+tar -czf ../$BUILD_NAME.tar.gz ./*
+
+
+###################################
+#   make skeleton                 #
+###################################
+mkdir $TMP_DIR -p
+cd $TMP_DIR;
+dh_make -f ../$BUILD_NAME.tar.gz -s -e ${DEBEMAIL} -c gpl2 -y --createorig -p $BUILD_NAME_ALT
+
+
+###################################
+#   edit 'debian/control' file    #
+###################################
+sed -i "s/#Vcs-Browser: http:\/\/git.debian.org\/?p=collab-maint\/${PROJECT_NAME}.git;a=summary/Vcs-Browser: ${PROJECT_SOURCE}/" debian/control
+sed -i "s/#Vcs-Git: git:\/\/git.debian.org\/collab-maint\/${PROJECT_NAME}.git/Vcs-Git: ${PROJECT_SOURCE}/" debian/control
+sed -i "s/<insert the upstream URL, if relevant>/${PROJECT_SOURCE}/" debian/control
+#sed -ri 's/^Depends: .*/Depends: /' debian/control
+sed -i 's/<insert up to 60 chars description>/Distributed and concurrency job manager/' debian/control
+sed -i 's/<insert long description, indented with spaces>/Distributed and concurrency job manager with cron syntax/' debian/control
+sed -i 's/Section: unknown/Section: utils/' debian/control
+sed -i 's/Build-Depends: debhelper (>= 8.0.0)/Build-Depends: debhelper (>= 9.0.0), golang (>= 1.9)/' debian/control
+sed -i 's/Standards-Version: 3.9.4/Standards-Version: 3.9.5/' debian/control
+
+
+###################################
+#   edit 'debian/copyright' file  #
+###################################
+sed -i "s/<url:\/\/example.com>/${PROJECT_SOURCE}/" debian/copyright
+sed -i "s/<years> <put author.s name and email here>/${DEBFULLNAME} <${DEBEMAIL}>/" debian/copyright
+sed -i 's/<years> <likewise for another author>/\n/' debian/copyright
+sed -ri 's/^#.+//' debian/copyright
+
+###################################
+#   edit 'debian/changelog' file  #
+###################################
+sed -i 's/unstable;/trusty;/' debian/changelog
+
+if [[ "$IS_TEST" != "1" ]];then
+    vi debian/changelog
+fi
+
+
+## Remove useless files
+rm debian/*.ex &>/dev/null
+rm debian/*.EX &>/dev/null
+#
+### build package (https://help.launchpad.net/Packaging/PPA/BuildingASourcePackage)
+#debuild -S -sa
+#
+### Upload to PPA
+#dput -d ppa:onm/shlanc ${WORK_DIR}/${BUILD_NAME_ALT}-1_source.changes
+#
+#rm -rf $WORK_DIR
+
